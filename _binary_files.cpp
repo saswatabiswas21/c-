620649b6f28c1f319ff44f04f5a316b5042ee86e
Saswata Biswas <87270823+saswatabiswas21@users.noreply.github.com>
2023-05-21 20:08:36 +0530

    Add files via upload

---
+++ _binary_files.cpp
@@ -0,0 +1,31 @@
+//i/o_operation_in_binary_files
+#include<iostream>
+#include<fstream>
+#include<iomanip>
+using namespace std;
+const char *filename ="BINARY";
+int main(){
+	float height[4]={175.5,153.0,167.25,160.70};
+	
+	ofstream outfile;
+	outfile.open(filename);
+	
+	outfile.write((char *)& height,sizeof(height));//read in file
+	outfile.close();
+	
+	for(int i=0;i<4;i++)
+		height[i]=0;
+		
+	ifstream infile;
+	infile.open(filename);
+	
+	infile.read((char *) & height,sizeof(height));//write in file
+	
+	for(int i=0;i<4;i++){
+		cout.setf(ios::showpoint);
+		cout<<setw(10)<<height[i];
+	}
+	infile.close();
+	
+	return 0;
+}
